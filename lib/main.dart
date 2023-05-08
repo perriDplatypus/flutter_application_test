4a19db2b698a3716adffc00bbd449fc0a222093e
Abhay <abhaytyagi2513@gmail.com>
2023-05-08 23:25:54 +1000

    add Favorites page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 
 
@@ -67,7 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
 				page = GeneratorPage();
 				break;
 			case 1:
-				page = Placeholder();
+				page = FavoritesPage();
 				break;
 			default:
 				throw UnimplementedError("No widget for $selectedIndex");
@@ -160,6 +159,34 @@ class GeneratorPage extends StatelessWidget {
 					),
 				],
 			),
+		);
+	}
+}
+
+
+class FavoritesPage extends StatelessWidget {
+	@override
+	Widget build(BuildContext context) {
+		var appState = context.watch<MyAppState>();
+
+		if (appState.favorites.isEmpty) {
+			return Center(
+				child: Text("No favorites yet... :)"),
+			);
+		}
+
+		return ListView(
+			children: [
+				Padding(
+					padding: const EdgeInsets.all(20),
+					child: Text("You have ${appState.favorites.length} favorites:")
+				),
+				for (var pair in appState.favorites)
+					ListTile(
+						leading: Icon(Icons.favorite),
+						title: Text(pair.asLowerCase),
+					),
+			],
 		);
 	}
 }
