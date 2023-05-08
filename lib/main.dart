eba16e4ec8520d2335193ba6fb8fa74154d2d9db
Abhay <abhaytyagi2513@gmail.com>
2023-05-08 21:55:48 +1000

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -42,13 +42,13 @@ class MyHomePage extends StatelessWidget {
 	@override
 	Widget build(BuildContext context) {
 		var appState = context.watch<MyAppState>();
+		var pair = appState.current;
 
 		return Scaffold(
 			body: Column(
 				children: [
 					Text('A random idea:'),
-					Text(appState.current.asLowerCase),
-
+					BigCard(pair: pair),
 					ElevatedButton(
 						onPressed: () {
 							appState.getNext();
@@ -58,5 +58,19 @@ class MyHomePage extends StatelessWidget {
 				],
 			),
 		);
+	}
+}
+
+
+class BigCard extends StatelessWidget {
+	const BigCard({
+		super.key,
+		required this.pair,
+	});
+	final WordPair pair;
+
+	@override
+	Widget build(BuildContext context) {
+		return Text(pair.asLowerCase);
 	}
 }
